70f5cd2e5dcdb4668f54c0fad6b262ef4b86fbb2
ChristianIlliano <grafica.illiano@gmail.com>
2024-03-25 15:54:56 +0100

    feat: output

--- js/script.js
+++ js/script.js
@@ -7,11 +7,24 @@ console.log(stringKm, typeof stringKm);
 const stringAge = prompt("quanti anni hai?"); //string
 console.log(stringKm, typeof stringKm);
 
-const numberKm = parseInt("stringKm") //number
+const numberKm = parseInt(stringKm) //number
 console.log(numberKm, typeof numberKm);
 
-const agePass = parseInt("stringAge") //number
+const agePass = parseInt(stringAge) //number
 console.log(agePass, typeof agePass);
 
-const priceTicket = numberKm * 0.21
+const priceTicket = numberKm * 0.21;
 console.log(priceTicket, typeof priceTicket);
+
+let result = result.toFixed(2)
+
+if (agePass < 18) {
+    result = priceTicket - 0.20
+    console.log("Prezzo scontato Minorenne");
+    
+} else if (agePass > 65) {
+    result = priceTicket - 0.40
+    console.log("Prezzo scontato Anziano");
+}
+
+document.getElementById("result").innerHTML = result
