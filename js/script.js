0818e436e263ee5664a88b4793b9d0f705a27382
ChristianIlliano <grafica.illiano@gmail.com>
2024-03-25 15:17:29 +0100

    add: input

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 // test
-alert ("ciao")
+// alert ("ciao")
+
+const stringKm = prompt("quanti km vuoi percorrere?"); //string
+console.log(stringKm, typeof stringKm);
+
+const stringAge = prompt("quanti anni hai?"); //string
+console.log(stringKm, typeof stringKm);
+
+const numberKm = parseInt("stringKm") //number
+console.log(numberKm, typeof numberKm);
+
+const agePass = parseInt("stringAge") //number
+console.log(agePass, typeof agePass);
+
+const priceTicket = numberKm * 0.21
+console.log(priceTicket, typeof priceTicket);
